1b9bfc9341a449b89e65ee42ab031d4fb7bdc9b0
Alexander <a_boichenko@netup.ru>
2020-11-02 14:49:36 +0300

    :ambulance: (HLS): Add support for multiple tracks of same type; ref T6222 Client software & devices
    Issue on github: https://github.com/google/ExoPlayer/issues/2014
    Original commit: https://github.com/stari4ek/ExoPlayer/commit/ce66b33059d28fe438973d73a9203d4f95e69d86
    
    Auditors: alex3d

--- library/extractor/src/main/java/com/google/android/exoplayer2/extractor/ts/TsExtractor.java
+++ library/extractor/src/main/java/com/google/android/exoplayer2/extractor/ts/TsExtractor.java
@@ -114,6 +114,9 @@ public final class TsExtractor implements Extractor {
   private static final int BUFFER_SIZE = TS_PACKET_SIZE * 50;
   private static final int SNIFF_TS_PACKET_COUNT = 5;
 
+  // Custom fix: https://github.com/google/ExoPlayer/issues/2014
+  public static boolean HLS_MULTIPLE_TRACKS_ALLOWED = true;
+
   private final @Mode int mode;
   private final int timestampSearchBytes;
   private final List<TimestampAdjuster> timestampAdjusters;
@@ -635,7 +638,8 @@ public final class TsExtractor implements Extractor {
         }
         remainingEntriesLength -= esInfoLength + 5;
 
-        int trackId = mode == MODE_HLS ? streamType : elementaryPid;
+        // Custom fix: https://github.com/google/ExoPlayer/issues/2014
+        int trackId = mode == MODE_HLS && !HLS_MULTIPLE_TRACKS_ALLOWED ? streamType : elementaryPid;
         if (trackIds.get(trackId)) {
           continue;
         }
